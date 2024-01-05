bbce8aaa33f991dcfe6951c6833e44d81900a359
zedhappened <zaid.iqbal.777@gmail.com>
2024-01-05 20:02:34 +0500

    added fonts and color palette to tailwinds, added an error page

---
+++ src/error-page.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useRouteError } from 'react-router-dom';
+
+
+const ErrorPage = () => {
+    const error = useRouteError();
+    console.error(error);
+
+    return (
+        <main className="animated-background flex items-center justify-center">
+            <div className="flex-column text-center">
+                <h1 className="font-bold from-stone-900 text-6xl py-6 font-alfaSlabOne text-primary">Oops!</h1>
+                <p className="font-roboto text-2xl font-bold">Sorry, an unexpected error has occurred.</p>
+                <p className="font-roboto text-xl text-secondary">
+                    Error Message: "{error.statusText || error.message}"
+                </p>
+            </div>
+        </main>
+    );
+}
+
+export default ErrorPage;

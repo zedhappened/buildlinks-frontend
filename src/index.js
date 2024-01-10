45781000c38f6b5ec4c433f5491959afbbdb22f7
zed <zaid.iqbal.777@gmail.com>
2024-01-10 14:27:18 +0500

    added suspense

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import { RouterProvider } from "react-router-dom";
 
 import router from './routes/routes';
 import "./styles.css";
+import Spinner from './spinner';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <React.Suspense fallback={<Spinner />}>
+      <RouterProvider router={router} />
+    </React.Suspense>
   </React.StrictMode>
 );
 
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
+import React from "react";
 
-import ErrorPage from "../error-page";
 import Home from "../pages/user/Home";
 import Categories from "../pages/user/Categories";
 import UserLayout from "../layouts/UserLayout";
 import AdminLayout from "../layouts/AdminLayout";
+const ErrorPage = React.lazy(() => import("../error-page"));
 
 const router = createBrowserRouter([
     {
